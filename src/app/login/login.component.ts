8065fb956ed9913dd2a9ca4a010cab4ee30e74cf
mangani <paulo.bruno@vianuvem.com.br>
2019-02-08 00:03:00 -0200

    paliativo para controlar o form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { UrlEnum } from '../enum/url.enum';
 import { DateType } from '../enum/date-type.enum';
 import { NgForm } from '@angular/forms';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-login',
@@ -25,6 +26,28 @@ export class LoginComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    this.loginForm
+      .valueChanges
+      .subscribe((form: {username: string, password: string, birthdate: string}) => {
+        console.warn('form -> ', form);
+        console.warn('loginForm antes validarNascimentoMangani', this.loginForm.invalid);
+        this.validarNascimentoMangani(form);
+        console.warn('loginForm apos validarNascimentoMangani', this.loginForm.invalid);
+    });
+  }
+
+  private validarNascimentoMangani(form: {username: string, password: string, birthdate: string}): void {
+
+    if (this.loginForm && this.loginForm.controls && this.loginForm.controls['birthdate']) {
+
+      if ('01/1978' !== form.birthdate) {
+        this.loginForm.controls['birthdate'].setErrors({'dataInvalida': 'dataInvalida'});
+
+      } else {
+        this.loginForm.controls['birthdate'].setErrors(null);
+
+      }
+    }
   }
 
   login(): void {
